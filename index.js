e34ede51b06b4d309e48373c5fb4232b1849b4d2
Jatin P <55746030+jatinp-mt@users.noreply.github.com>
2023-02-20 15:49:00 +0530

    Code refactor
    Code refactor

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ const login = () => {
             console.log(`Hello, ${answer}!`);
 
             // Check the user is already available?
-            const user = data.find(o => o.name === answer);
+            const user = getCurrentUser(answer);
 
             if (!user) {
                 // Print answer
@@ -75,48 +75,12 @@ const deposit = () => {
     // ask user for deposit amount.
     rl.question('Enter deposit amount: ', (amount) => {
         // Find and add deposit amount to bank balance of the user.
-        const user = data.find(o => o.name === loggedInUser);
+        const user = getCurrentUser(loggedInUser);
 
         // Check is the depositer is owed to someone
         if (user?.owed?.isOwed) {
-            // Find the owed to user.
-            const owedToUser = data.find(o => o?.name !== user?.owed?.owedFrom);
-
-            // Check the deposite amount is smaller then current balance
-            if (user.balance < amount) {
-
-                if (Number(user?.owed?.owedAmount) < Number(amount)) {
-
-                    // First deadict the amount from the logged-in user from bank balance.
-                    user.owed.isOwed = false;
-                    user.balance = Number(amount) - Number(user?.owed?.owedAmount);
-                    user.owed.owedAmount = 0;
-
-                    // Print answers
-                    console.log(`Transferred $${Number(amount) - Number(user?.balance)} to ${owedToUser.owed.owedTo}`);
-                    console.log(`Your Balance is $${user.balance}`);
-
-                } else {
-                    // Owed amount 
-                    const owedAmount = Number(user?.owed?.owedAmount) > Number(amount) ? Number(user?.owed?.owedAmount) - Number(amount) : Number(amount) - Number(user?.owed?.owedAmount);
-
-                    // First deadict the amount from the logged-in user from bank balance.
-                    user.owed.isOwed = true;
-                    user.balance = 0;
-                    user.owed.owedAmount = owedAmount;
-
-                    // Update the receiver user
-                    owedToUser.owed.owedAmount = owedAmount;
-
-                    // Add the amount to the receiver bank balance.
-                    owedToUser.balance = Number(owedToUser.balance) + Number(amount)
-
-                    // Print answers
-                    console.log(`Transferred $${amount} to ${owedToUser.owed.owedTo}`);
-                    console.log(`Your Balance is $${user.balance}`);
-                    console.log(`Owed $${owedAmount} to ${owedToUser.owed.owedFrom}`);
-                }
-            }
+            // If the dpositer is an owed user
+            depositIfOwedUser(user, amount);
         } else {
             // Calculate and update the balance
             user.balance = Number(user.balance) + Number(amount);
@@ -132,7 +96,7 @@ const withDraw = () => {
     // ask user for withdraw amount.
     rl.question('Enter withdraw amount: ', (amount) => {
         // Find and add deposit amount to bank balance of the user.
-        const user = data.find(o => o.name === loggedInUser);
+        const user = getCurrentUser(loggedInUser);
 
         if (Number(user.balance) >= Number(amount)) {
             // Calculate and update the balance
@@ -154,80 +118,15 @@ const transfer = () => {
     // ask user for deposit amount.
     rl.question('Enter user name to initiate transfer: ', (name) => {
         // Find and add deposit amount to bank balance of the user.
-        const user = data.find(o => o.name === name);
-        const loginUser = data.find(o => o.name === loggedInUser);
+        const user = getCurrentUser(name);
+        const loginUser = getCurrentUser(loggedInUser);
 
         if (!user) {
             console.log(`Sorry!, user not found!`);
         } else if (loginUser.balance === 0) {
             console.log(`Insufficient balance!`);
         } else {
-            // Ask the amount to be transfered.
-            rl.question('Please enter amount: ', (amount) => {
-                // Find and first check the available balance and check is the user already owed.
-                const user = data.find(o => o.name === loggedInUser);
-
-                // Find the user in which we have to add amount in their available balance.
-                const receiverUser = data.find(o => o.name === name);
-
-                if (user.balance < amount && !user?.owed?.isOwed) {
-                    // Owed amount 
-                    const owedAmount = Number(amount) - Number(user.balance);
-
-                    // transferd amount
-                    const transferedAount = Number(amount) - Number(owedAmount);
-
-                    // First deadict the amount from the logged-in user from bank balance.
-                    user.balance = 0;
-
-                    // Owed amount, to user and from user to the depositer 
-                    user.owed.isOwed = true;
-                    user.owed.owedAmount = owedAmount;
-                    user.owed.owedTo = name;
-                    user.owed.owedFrom = loggedInUser;
-
-                    // Owed amount, to user and from user to the receiver
-                    receiverUser.owed.isOwed = true;
-                    receiverUser.owed.owedAmount = owedAmount;
-                    receiverUser.owed.owedTo = name;
-                    receiverUser.owed.owedFrom = loggedInUser;
-
-                    // Add the amount to the receiver bank balance.
-                    receiverUser.balance = Number(receiverUser.balance) + Number(amount)
-
-                    // Print answer
-                    console.log(`Transferred $${transferedAount} to ${name}`);
-                    console.log(`Your Balance is $${user.balance}`);
-                    console.log(`Owed $${owedAmount} to ${name}`);
-
-                } else {
-                    if (loginUser.owed.isOwed) {
-                        // Update the balance
-                        loginUser.balance = Number(loginUser.balance) - Number(amount);
-                        receiverUser.balance = !receiverUser.owed.isOwed ? Number(receiverUser.balance) + Number(amount) : 0;
-
-                        // update the owed amount
-                        const owedAmount = Number(loginUser.owed.owedAmount) - Number(amount);
-                        loginUser.owed.owedAmount = owedAmount;
-                        receiverUser.owed.owedAmount = owedAmount;
-
-                        // Print answer
-                        console.log(`Transferred $${amount} to ${name}`);
-                        console.log(`Your Balance is $${user.balance}`);
-                        console.log(`Owed $${owedAmount} from ${receiverUser.name}`);
-                    } else {
-                        // First deadict the amount from the logged-in user from bank balance.
-                        user.balance = Number(user.balance) - Number(amount);
-
-                        // Add the amount to the receiver bank balance.
-                        receiverUser.balance = Number(receiverUser.balance) + Number(amount)
-
-                        // Print answer
-                        console.log(`Transferred $${amount} to ${name}`);
-                        console.log(`Your Balance is $${user.balance}`);
-                    }
-                }
-            })
+            initialTransfer(user, loginUser, name);
         }
     });
 }
@@ -235,7 +134,7 @@ const transfer = () => {
 // Check balance
 const checkBalance = () => {
     // Find the user and check available balance.
-    const user = data.find(o => o.name === loggedInUser);
+    const user = getCurrentUser(loggedInUser);
     console.log(`Your balance is, ${user.balance}!`);
 }
 
@@ -249,8 +148,119 @@ const logout = () => {
     printOption();
 }
 
+// Find current user with all data
+const getCurrentUser = (userName) => {
+    return data.find(o => o.name === userName);
+}
+
+// Initiate transfer
+const initialTransfer = (user, loginUser, name) => {
+    // Ask the amount to be transfered.
+    rl.question('Please enter amount: ', (amount) => {
+        if (loginUser.balance < amount && !loginUser?.owed?.isOwed) {
+            // Owed amount 
+            const owedAmount = Number(amount) - Number(loginUser.balance);
+
+            // transferd amount
+            const transferedAount = Number(amount) - Number(owedAmount);
+
+            // First deduct the amount from the logged-in user from bank balance.
+            loginUser.balance = 0;
+
+            // Owed amount, to user and from user to the depositer 
+            loginUser.owed.isOwed = true;
+            loginUser.owed.owedAmount = owedAmount;
+            loginUser.owed.owedTo = name;
+            loginUser.owed.owedFrom = loggedInUser;
+
+            // Owed amount, to user and from user to the receiver
+            user.owed.isOwed = true;
+            user.owed.owedAmount = owedAmount;
+            user.owed.owedTo = name;
+            user.owed.owedFrom = loggedInUser;
+
+            // Add the amount to the receiver bank balance.
+            user.balance = Number(user.balance) + Number(amount)
+
+            // Print answer
+            console.log(`Transferred $${transferedAount} to ${name}`);
+            console.log(`Your Balance is $${loginUser.balance}`);
+            console.log(`Owed $${owedAmount} to ${name}`);
+        } else {
+            if (loginUser.owed.isOwed) {
+                // Update the balance
+                loginUser.balance = Number(loginUser.balance) - Number(amount);
+                user.balance = !user.owed.isOwed ? Number(user.balance) + Number(amount) : 0;
+
+                // update the owed amount
+                const owedAmount = Number(loginUser.owed.owedAmount) - Number(amount);
+                loginUser.owed.owedAmount = owedAmount;
+                user.owed.owedAmount = owedAmount;
+
+                // Print answer
+                console.log(`Transferred $${amount} to ${name}`);
+                console.log(`Your Balance is $${loginUser.balance}`);
+                console.log(`Owed $${owedAmount} from ${user.name}`);
+            } else {
+                // First deduct the amount from the logged-in user from bank balance.
+                loginUser.balance = Number(loginUser.balance) - Number(amount);
+
+                // Add the amount to the receiver bank balance.
+                user.balance = Number(user.balance) + Number(amount)
+
+                // Print answer
+                console.log(`Transferred $${amount} to ${name}`);
+                console.log(`Your Balance is $${loginUser.balance}`);
+            }
+        }
+    })
+}
+
+// Deposite if the depositer is owed
+const depositIfOwedUser = (user, amount) => {
+    // Find the owed to user.
+    const owedToUser = data.find(o => o?.name !== user?.owed?.owedFrom);
+
+    // Check the deposite amount is smaller then current balance
+    if (user.balance < amount) {
+
+        if (Number(user?.owed?.owedAmount) < Number(amount)) {
+
+            // First deduct the amount from the logged-in user from bank balance.
+            user.owed.isOwed = false;
+            user.balance = Number(amount) - Number(user?.owed?.owedAmount);
+            user.owed.owedAmount = 0;
+
+            // Print answers
+            console.log(`Transferred $${Number(amount) - Number(user?.balance)} to ${owedToUser.owed.owedTo}`);
+            console.log(`Your Balance is $${user.balance}`);
+
+        } else {
+            // Owed amount 
+            const owedAmount = Number(user?.owed?.owedAmount) > Number(amount) ? Number(user?.owed?.owedAmount) - Number(amount) : Number(amount) - Number(user?.owed?.owedAmount);
+
+            // First deduct the amount from the logged-in user from bank balance.
+            user.owed.isOwed = true;
+            user.balance = 0;
+            user.owed.owedAmount = owedAmount;
+
+            // Update the receiver user
+            owedToUser.owed.owedAmount = owedAmount;
+
+            // Add the amount to the receiver bank balance.
+            owedToUser.balance = Number(owedToUser.balance) + Number(amount)
+
+            // Print answers
+            console.log(`Transferred $${amount} to ${owedToUser.owed.owedTo}`);
+            console.log(`Your Balance is $${user.balance}`);
+            console.log(`Owed $${owedAmount} to ${owedToUser.owed.owedFrom}`);
+        }
+    }
+}
+
 // Display welcome message and available options
 printOption();
+
 
 
 // Listen for user input
